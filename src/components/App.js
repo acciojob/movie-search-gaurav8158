26bb9c6ffcb9d4865aee6d3e3bd0383602a70664
Gaurav Soni <gsoni8158@gmail.com>
2023-05-29 02:04:02 +0530

    submitting code-Mon May 29 2023 02:04:01 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
-
 import React from "react";
-import './../styles/App.css';
+ import './../styles/App.css';
+import Search from "./Search.js";
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <Search/>
     </div>
   )
 }
